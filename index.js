6e9eac8297355692d82b7c69e431ceea5ee1cebe
tonipal <86857995+tonipal@users.noreply.github.com>
2022-12-13 15:52:55 +0200

    reusable function

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 
-let caloriesArray = [0];
-const allFileContents = fs.readFileSync('caloriesOnElf.txt', 'utf-8');
-allFileContents.split(/\r?\n/).forEach(line =>  {
-    if (line == "") {
-        caloriesArray.push(0);
-    } else {
-        caloriesArray[caloriesArray.length -1] += Number(line);
-    }
-});
+const getHighestCalorieScore = () => {
+    let caloriesArray = [0];
+    const allFileContents = fs.readFileSync('caloriesOnElf.txt', 'utf-8');
+    allFileContents.split(/\r?\n/).forEach(line =>  {
+        if (line == "") {
+            caloriesArray.push(0);
+        } else {
+            caloriesArray[caloriesArray.length -1] += Number(line);
+        }
+    });
+    return Math.max(...caloriesArray);
+}
+
+console.log(getHighestCalorieScore());
 
 
-console.log(Math.max(...caloriesArray));
 
