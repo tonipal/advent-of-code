01ade91b45f4c42c045fdef90606ce3ab66dd84f
tonipal <palmola.toni@gmail.com>
2022-12-13 12:41:45 +0200

    day01p1 completed

--- 2022/day01p1.js
+++ 2022/day01p1.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 let caloriesArray = [0];
-const allFileContents = fs.readFileSync('caloriesOnElf.txt', 'utf-8');
+const allFileContents = fs.readFileSync('../caloriesOnElf.txt', 'utf-8');
 allFileContents.split(/\r?\n/).forEach(line =>  {
     if (line == "") {
         caloriesArray.push(0);
@@ -12,4 +12,3 @@ allFileContents.split(/\r?\n/).forEach(line =>  {
 
 
 console.log(Math.max(...caloriesArray));
-
