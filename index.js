305b23f5084f797ce3d43552b68601f3821dfe00
tonipal <palmola.toni@gmail.com>
2022-12-14 15:04:52 +0200

    startpoint to day3 challenge

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
 const fs = require('fs');
 
-const playRockPaperScissors = () => {
-    let totalScore = 0;
+const rearrangeRucksack = () => {
+    const itemPriorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    const loss = 0;
-    const draw = 3;
-    const win = 6;
-
-    const playerChoseRock = 1;
-    const playerChosePaper = 2;
-    const playerChoseScissors = 3;
-
-    const allFileContents = fs.readFileSync('strategyGuide.txt', 'utf-8');
+    const allFileContents = fs.readFileSync('itemsInsideRucksack.txt', 'utf-8');
     allFileContents.split(/\r?\n/).forEach(line =>  {
-        if (line[2] === 'X' && line[0] === 'A') {
-            totalScore += loss + playerChoseScissors
-        }
-        if(line[2] === 'X' && line[0] === 'B') {
-            totalScore += loss + playerChoseRock
-        }
-        if(line[2] === 'X' && line[0] === 'C') {
-            totalScore += loss + playerChosePaper
-        }
-        if(line[2] === 'Y' && line[0] === 'A') {
-            totalScore += draw + playerChoseRock
-        }
-        if(line[2] === 'Y' && line[0] === 'B') {
-            totalScore += draw + playerChosePaper
-        }
-        if(line[2] === 'Y' && line[0] === 'C') {
-            totalScore += draw + playerChoseScissors
-        }
-        if(line[2] === 'Z' && line[0] === 'A') {
-            totalScore += win + playerChosePaper
-        }
-        if(line[2] === 'Z' && line[0] === 'B') {
-            totalScore += win + playerChoseScissors
-        }
-        if(line[2] === 'Z' && line[0] === 'C') {
-            totalScore += win + playerChoseRock
-        }
+        
     });
-    return totalScore;
 }
 
-console.log(playRockPaperScissors());
+console.log(rearrangeRucksack());
 
