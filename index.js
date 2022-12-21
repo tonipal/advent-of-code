133206a10573085cdf0aa20a7491930ca123085b
tonipal <86857995+tonipal@users.noreply.github.com>
2022-12-21 11:41:11 +0200

    day08 input

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const fs = require('fs');
 
-const findMarker = () => {
+const findVisibleTrees = () => {
     const allFileContents = fs.readFileSync('datastream.txt', 'utf-8');
     const input = allFileContents;
-
-    for (let i = 0; i < input.length; i++) {
-        let aggregator = "";
-        let marker = [];
-        let unique = [];
-    
-        for (let j = 0; j < 14; j++) {
-            aggregator += input[i+j];
-        }
-        marker = aggregator.split("");
-        unique = Array.from(new Set(marker));
-        if (marker.length === unique.length) {
-            return i + 14;
-        }
-    }
 }
 
-console.log(findMarker());
+console.log(findVisibleTrees());
 
 
